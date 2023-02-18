2b0245783c8dabf84950aded91c284c357026b9e
lisacharuma <charumalisah@gmail.com>
2023-02-17 19:10:30 -0800

    Print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,46 +1,4 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- * Description - Alphabet soup
- * Return: 0
- * */
-int main(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		if (c != 'e' && c != 'q')
-		{
-			putchar(c);
-		}
-	}
-	putchar('\n');
-
-	return (0);
-}#include <stdio.h>
-
-/**
- * main - Entry point
- * Description - Alphabet soup
- * Return: 0
- * */
-int main(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		if (c != 'e' && c != 'q')
-		{
-			putchar(c);
-		}
-	}
-	putchar('\n');
-
-	return (0);
-}#include <stdio.h>
+#include <stdio.h>;
 
 /**
  * main - Entry point
