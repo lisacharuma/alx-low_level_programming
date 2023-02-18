e6822825373c0374abfee70e3cba6d46237a10f7
lisacharuma <charumalisah@gmail.com>
2023-02-18 01:33:25 -0800

    4 Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - prints text using puts
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
