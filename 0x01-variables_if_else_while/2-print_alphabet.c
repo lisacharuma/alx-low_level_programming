1e2934d783db012bbf7b69eaea9fdc1f4a63207a
lisacharuma <charumalisah@gmail.com>
2023-02-19 04:54:46 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - Prints alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+	}
+
+}
