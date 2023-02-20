9770bb299b090eed6fda42f6ad3e504771aed602
lisacharuma <charumalisah@gmail.com>
2023-02-19 18:05:00 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - base  in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	charc = 'a';
+	int b;
+
+	while (b < 10)
+	{
+		putchar(b + '0');
+		b++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
