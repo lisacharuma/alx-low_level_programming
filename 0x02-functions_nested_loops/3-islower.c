d5db611cf21b7aa332ac25b49ba237b5703f228d
lisacharuma <charumalisah@gmail.com>
2023-02-21 08:22:17 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - takes parameter int c
+ * Description - checks whether a given parameter is lower or not
+ * Return: 1 if letter is lowercase and  0 if otherwise
+ */
+
+int _islower(int c)
+{
+	ch c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	else
+		return (0);
+}
