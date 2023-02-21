3b19e30de954e0c18c134f02355e7ad707d03b42
lisacharuma <charumalisah@gmail.com>
2023-02-21 06:12:44 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - entry point
+ * Description - prints _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x[] = "_putchar";
+	int i;
+
+	for (i = 0; x[i]; i++)
+	{
+		putchar(x[i])
+	}
+	putchar('\n);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a readme file for the new project where we are learning functions in C as well as nested loops. We are also using prototypes which is fairly fun.
