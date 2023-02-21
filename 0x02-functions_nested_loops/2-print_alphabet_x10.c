dff4a5182d2cd82c9cc0ffd47a515f9ce8d80bc7
lisacharuma <charumalisah@gmail.com>
2023-02-21 06:41:21 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * return : 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
