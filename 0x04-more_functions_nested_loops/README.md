efcaee9fe40ee375788b36f58741c4d0ec20f125
lisacharuma <charumalisah@gmail.com>
2023-02-23 08:05:10 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the readme file for the project 0x04-more_functions_nested_loops where we are learning more about functions and loops. Its challenging and fun.
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef Main_h
+#define Main_h
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
