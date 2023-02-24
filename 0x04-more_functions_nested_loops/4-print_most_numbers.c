00b0e7bf127bafc3809bf19e62b6c909f54112b0
lisacharuma <charumalisah@gmail.com>
2023-02-23 17:49:42 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ * Description: prints numbers up to 9 except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
