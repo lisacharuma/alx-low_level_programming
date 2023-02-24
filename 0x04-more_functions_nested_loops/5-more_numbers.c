72fc43c2bbb1545243f77a3117beabde61d0cd40
lisacharuma <charumalisah@gmail.com>
2023-02-23 18:41:03 -0800

    changes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ * Prints 10 times the numbers from 0 to 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar((j / 10) + '0');
+		}
+		_putchar((j % 10) + '0');
+	}
+	_putchar('\n');
+}
