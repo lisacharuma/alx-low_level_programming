f2dcde241bcdd921e603d7294193385a8dd37d3f
lisacharuma <charumalisah@gmail.com>
2023-02-25 07:03:40 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,21 +13,23 @@ void print_triangle(int size)
 	int j;
 	int k;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
-		return;
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++);
+			{
+				_putchar(32);
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = 1; j <= size - i; j++);
-		{
-			_putchar(32);
-		}
-		for (k = 1; k <= i; k++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
+		_putchar('\n')
 	}
 }
