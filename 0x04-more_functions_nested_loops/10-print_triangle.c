a10963bf5c9f14d24d7fde663cc492b8f7fad528
lisacharuma <charumalisah@gmail.com>
2023-02-25 06:40:59 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - entry point
+ * Description: prints a triangle
+ * @size: size of the triangle
+ * Return: integer
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++);
+		{
+			_putchar(32);
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
