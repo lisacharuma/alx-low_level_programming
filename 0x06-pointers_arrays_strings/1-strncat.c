7975df6ff0b9aa9b7193ce89d1fe2176bb09a4fa
lisacharuma <charumalisah@gmail.com>
2023-03-02 19:08:03 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,16 +11,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char *p = dest;
+	int index = 0; dest_len = 0;
 
-	while (*p != '\0')
+	while (dest[index++])
 	{
-		p++;
+		dest_len++;
 	}
-	while (src != '\0' && n-- > 0)
+	for (index = 0; src[index] && index < n; index++)
 	{
-		p++ = *src++;
+		dest[dest_len++] = src[index];
 	}
-	*p = '\0';
 	return (dest);
 }
