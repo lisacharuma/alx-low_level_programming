b71b77d9b459039e0ff0bc867d496c87241ea252
lisacharuma <charumalisah@gmail.com>
2023-03-03 04:37:39 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *n);
 char *cap_string(char *str);
 char *leet(char *n);
-char *rot13(char *);
+char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
