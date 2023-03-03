01138726bff4fa872692cb1eefb842ed7cd082e0
lisacharuma <charumalisah@gmail.com>
2023-03-03 03:48:29 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (str[index] >= 'a' && str[index] <= 'z')
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 		{
 			index++;
 		if (str[index - 1] == ' ' ||
