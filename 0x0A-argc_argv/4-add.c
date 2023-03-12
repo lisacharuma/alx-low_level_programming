9830acc74ce42eb696ee03b3eb7eef71e11253b3
lisacharuma <charumalisah@gmail.com>
2023-03-12 07:22:21 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: add positive numbers
+ * @argc: arguments count
+ * @argv: arguments array (vector)
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	sum = 0; /* it will increase as more ints are passed*/
+
+	if (argc == 1)
+	{
+		printf("\n"); /* no arg passed*/
+		return (0);
+	}
+	for (i = 1; i < argc; i++) /* as long as there r mo than 1 args*/
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j])) /* if arg is !digit*/
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);/*increment sum */
+	}
+	printf("%d\n", sum);
+	return (0);
+}
