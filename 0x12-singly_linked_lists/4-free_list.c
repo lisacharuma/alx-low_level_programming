d3dcf4fe1b6d3a8b843ee1e3bae6b9d01dfb2b7e
lisacharuma <charumalisah@gmail.com>
2023-04-06 08:10:28 -0700

    A function that frees a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/*
+/**
  * free_list - new function
  * Description: frees a linked list
  * @head: pointer to the first node in the list
@@ -9,12 +9,13 @@
 
 void free_list(list_t *head)
 {
-	list_t *this_node = head; /*starts at where head is pointing*/
+	list_t *this_node;
 
 	while (head != NULL) /*while the node isn't the list terminator*/
 	{
-		this_node = head; /*start at the beginning of list*/
-		head = head->next; /*mv 2 the nxt node*/
-		free(this_node); /*free current node*/
+		this_node = head->next; /*set head's next to new node*/
+		free(head->str); /*free str space*/
+		free(head); /*free current node*/
+		head = this_node; /*make it the new head*/
 	}
 }
