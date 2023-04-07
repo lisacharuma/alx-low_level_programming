c6bda4fbed5a136cdfb4a9d7d36348927627dd89
lisacharuma <charumalisah@gmail.com>
2023-04-06 23:39:50 -0700

    A function that prints a given line

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_line - new function
+ * Description: prints a given line
+ * Return: void
+ */
+
+void print_line(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
