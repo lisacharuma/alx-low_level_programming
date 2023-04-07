d6de4c42cd2b1de3ff17d6f80c584da4f96403bc
lisacharuma <charumalisah@gmail.com>
2023-04-07 08:20:44 -0700

    A function that adds new node at given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head; /*poibt 2 e previous head*/
 		*head = new_node; /*new node is the head*/
-		return (*head);
+		return (new_node);
 	}
 	else
 	{
