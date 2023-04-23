b82b9dd4b969496f62429881fa995568c9a70bf3
lisacharuma <charumalisah@gmail.com>
2023-04-23 00:27:32 -0700

    A function that copies file contents to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: array of arguments
+ * Return: 0 on success, or an error code on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (97);
+	}
+	return (copy_file(argv[1], argv[2]));
+}
+
+/**
+ * copy_file - new function
+ * Description: copies a file from one folder to another
+ * @file_from_path: file source
+ * @file_to_path: file destination
+ * Return: int
+ */
+
+int copy_file(char *file_from_path, char *file_to_path)
+{
+	int file_from, file_to, bytes_read, bytes_written;
+	char *buffer;
+
+	file_from = open(file_from_path, O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from_path);
+		return (98);
+	}
+	file_to = open(file_to_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to_path);
+		return (99);
+	}
+	buffer = malloc(1024 * sizeof(char));
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't allocate memory\n");
+		return (100);
+	}
+	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
+	{
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to_path);
+			return (99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from_path);
+		return (98);
+	}
+	free(buffer);
+	if (close(file_from) == -1 || close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		return (100);
+	}
+	return (0);
+}
