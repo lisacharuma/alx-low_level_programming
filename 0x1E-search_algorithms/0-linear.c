5bddc5d2329be15fb9e63df9b08f69db754562a8
lisacharuma <charumalisah@gmail.com>
2024-02-06 12:18:34 +0200

    A function that uses linear search algorithm to seach for a number

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,14 +10,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t x;
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
-	for (x = 0; x < size; x++)
+	for (i = 0; i < size; i++)
 	{
-		if (array[x] == value)
-			return (x);
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
